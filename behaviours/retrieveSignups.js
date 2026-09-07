3d531e75889f50263788f151f27106ad6a14e55d
auvtre <auvtre.art@gmail.com>
2026-09-07 19:58:24 +0200

    feat: add retrieveSignups behaviour that retrieves all signups for the current skins

---
+++ behaviours/retrieveSignups.js
@@ -0,0 +1,61 @@
+getActiveAccents();
+
+function getActiveAccents() {
+    const forms = document.querySelectorAll("form[data-accent]");
+
+    forms.forEach((form) => {
+        const id = form.getAttribute("data-accent");
+        retrieveBySkinId(id);
+    });
+}
+
+function buildSkinTable(id, signups) {
+    const table = document.querySelector(`table[data-accent='${id}']`);
+    const runIds = [...new Set(signups.map((obj) => obj.RunId))];
+    let tr_signups = [];
+
+    runIds.forEach((run, runIndex) => {
+        // create a header for the run
+        const tr_th = table.querySelector("tr");
+        const th = document.createElement("th");
+        th.appendChild(document.createTextNode(`Run: ${runIndex + 1}`));
+        tr_th.appendChild(th);
+
+        // get signups for the run
+        const runSignups = [...signups.filter((obj) => obj.RunId === run)];
+
+        runSignups.forEach((signup, index) => {
+            let tr;
+            if (runIndex === 0) {
+                tr = document.createElement("tr");
+                table.appendChild(tr);
+                tr_signups[index] = tr;
+            } else {
+                tr = tr_signups[index];
+            }
+
+            td = document.createElement("td");
+            let content;
+            if (signup.IsPaid == 1) {
+                content = document.createTextNode(`${index + 1}. ${signup.Username} (PAID)`);
+            } else {
+                content = document.createTextNode(`${index + 1}. ${signup.Username}`);
+            }
+            td.appendChild(content);
+            tr.appendChild(td);
+        });
+    });
+}
+
+function retrieveBySkinId(id) {
+    const request = new Request(`https://signup-retrieval-worker.auvtrefraccents.workers.dev/?accentId=${id}`, {
+        method: "GET",
+    });
+
+    fetch(request).then((response) => {
+        response.json().then((json) => {
+            var signups = JSON.parse(json);
+            buildSkinTable(id, signups);
+        });
+    });
+}
