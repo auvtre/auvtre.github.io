9a723ea278f356cf4f2bef23137e361afad65555
auvtre <auvtre.art@gmail.com>
2026-09-08 13:29:49 +0200

    feat: add error message display

--- behaviours/addUser.js
+++ behaviours/addUser.js
@@ -6,12 +6,18 @@ document.querySelectorAll("form[data-accent]").forEach((form) => {
 
 function addUser(e) {
     e.preventDefault();
+    clearErrors();
     const accent = e.currentTarget.dataset.accent;
-    const inputUsername = e.currentTarget.querySelector(".signup-username").value;
+    let inputUsername = e.currentTarget.querySelector(".signup-username").value;
+    inputUsername = inputUsername.replace(/\s/g, "");
 
-    if (!accent) return;
+    // this should never happen unless i messed up
+    if (!accent) {
+        return;
+    }
 
     if (!validateUser(inputUsername)) {
+        displayError(accent, `Provided username '${inputUsername}' is not valid`);
         return;
     }
 
@@ -28,6 +34,8 @@ function addUser(e) {
             if (json.success) {
                 clearTable(accent);
                 retrieveBySkinId(accent);
+            } else {
+                displayError(accent, json.message);
             }
         });
     });
@@ -39,39 +47,9 @@ function validateUser(username) {
     // between 3 and 16 characters
     if (username.length > 16 || username.length < 3) return false;
 
-    const specialChars = [
-        "!",
-        "@",
-        "#",
-        "$",
-        "%",
-        "^",
-        "&",
-        "*",
-        "(",
-        ")",
-        "-",
-        "+",
-        "=",
-        "{",
-        "}",
-        "[",
-        "]",
-        ":",
-        ";",
-        '"',
-        "'",
-        "<",
-        ">",
-        ",",
-        ".",
-        "?",
-        "/",
-        "|",
-        "\\",
-    ];
+    const hasInvalidChars = /[^a-zA-Z0-9]/.test(username);
 
-    if (username.split("").some((c) => specialChars.includes(c))) return false;
+    if (hasInvalidChars) return false;
 
     return true;
 }
@@ -81,4 +59,22 @@ function clearTable(skinId) {
     const thead = document.createElement("thead");
     const tbody = document.createElement("tbody");
     table.replaceChildren(thead, tbody);
+}
+
+function clearErrors() {
+    const errorDivs = document.querySelectorAll(`div.active-error`);
+    errorDivs.forEach((div) => {
+        div.innerHTML = "";
+        div.setAttribute("class", "inactive-error");
+    });
+}
+
+function displayError(skinId, message) {
+    const errorDiv = document.querySelector(`div[data-error-accent='${skinId}']`);
+    errorDiv.setAttribute("class", "active-error");
+    const h4 = document.createElement("h4");
+    h4.innerText = "Sorry, your signup couldn't be completed";
+    const errorText = document.createElement("p");
+    errorText.innerText = message;
+    errorDiv.append(h4, errorText);
 }
